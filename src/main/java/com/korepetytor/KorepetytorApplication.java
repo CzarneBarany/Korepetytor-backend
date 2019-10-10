0df74ff6734c03e3ee87e9627f1186ccd89620ca
Robert Witczyk <r.witczyk@wp.pl>
2019-10-10 17:19:24 +0200

    Init project

---
+++ src/main/java/com/korepetytor/KorepetytorApplication.java
@@ -0,0 +1,13 @@
+package com.korepetytor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KorepetytorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KorepetytorApplication.class, args);
+	}
+
+}
